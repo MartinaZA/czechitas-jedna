d20e000289fb10258ca43e67e5c48aa10724d470
MartinaZA <martina.durdikova@gmail.com>
2022-02-17 22:08:08 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-let meno = "Martina";
+let meno = 'Martina';
 let vek = 38;
-console.log (meno, vek);
+console.log(meno, vek);
 let jeZivy = true;
 let jeMuz = false;
 let priemer = 3.48;
-console.log (jeZivy);
+console.log(jeZivy);
 import './ukol1';
-meno = "Jozef";
-let a = 1, b = 2 , c = a + b;
+meno = 'Jozef';
 
+let a = 1, b = 2;
+let c = a + b;
+console.log(c);
+
+let d = 4;
+console.log(d);
+
+console.log(`toto je ${meno}`);
 
 
 
