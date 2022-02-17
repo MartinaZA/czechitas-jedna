db65c033a6c0ce8d9e29e54ee6a7714684d39df1
MartinaZA <martina.durdikova@gmail.com>
2022-02-17 21:34:10 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const meno = "Martina";
+let meno = "Martina";
 let vek = 38;
 console.log (meno, vek);
 let jeZivy = true;
@@ -7,6 +7,8 @@ let priemer = 3.48;
 console.log (jeZivy);
 import './ukol1';
 meno = "Jozef";
+let a = 1, b = 2 , c = a + b;
+
 
 
 
