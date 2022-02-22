4f899b6618e97dc85f5dc580011988abe52e3e72
MartinaZA <martina.durdikova@gmail.com>
2022-02-22 18:57:54 +0100

    1 file modified

--- promenne/ukol2.js
+++ promenne/ukol2.js
@@ -12,4 +12,6 @@ let rokNarozeniZeny = 1984;
 let mesicNarozeniMuze = 5;
 let mesicNarozeniZeny = 4;
 let rozdiel = (rokNarozeniMuze * 12 + mesicNarozeniMuze) - (rokNarozeniZeny * 12 + mesicNarozeniZeny);
-console.log( muz + ' a ' + zena + ' su od seba vekove vzdialeni ' + rozdiel + ' mesiacov.')
+console.log( muz + ' a ' + zena + ' su od seba vekove vzdialeni ' + rozdiel + ' mesiacov.');
+
+
