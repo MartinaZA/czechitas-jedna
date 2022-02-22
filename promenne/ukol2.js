5d70e49aaf1a76ea5cca2aa3a8773ffffc027cc6
MartinaZA <martina.durdikova@gmail.com>
2022-02-22 19:04:58 +0100

    1 file modified, 1 added

--- promenne/ukol2.js
+++ promenne/ukol2.js
@@ -1,10 +1,3 @@
-Nadefinujme si proměnné a jejich hodnoty potřebné k uložení následujících informací:
- Jméno muže a ženy ​
- Rok a měsíc narození muže i ženy​
-Spočteme rozdíl věku muže a ženy v měsících a vypíšeme ve formátu​ "[muz] a [zena] jsou od sebe věkově vzdáleni [XY] měsíců​"
-Pro zjednodušení úkolu počítejme, že je muž vždy starší! (ať nemusíme řešit absolutní hodnotu z Math.abs())
-
-
 let muz = 'Jozef';
 let zena = 'Martina';
 let rokNarozeniMuze = 1982;
