7e941aeee29e2dad8c2fc0313e96bfd64ec52e39
MartinaZA <martina.durdikova@gmail.com>
2022-02-22 18:13:53 +0100

    1 file modified

--- promenne/ukol1.js
+++ promenne/ukol1.js
@@ -1,8 +1,12 @@
 let meno = "Martina";
 let vek = 37;
-let vaha = 68kg;
-let vyska = 1,77m;
+let vaha = 68;
+let vyska = 1.77;
+let BMI = vaha / vyska **2;
 
-console.log(vaha / vyska **2);
+
+//Jmenuji se Petra, je mi 34 let, vážím 65kg,  měřím 1.67m a moje BMI je 23,2.
+
+console.log('Volam sa ' + meno + ' , mam ' + vek + ' rokov, vazim ' + vaha + ' kg, meriam ' + vyska + ' m a moje BMI je ' + BMI + ' .' );
 
 
