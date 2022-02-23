aab9e0074e5849853f891c2c16b4626da65f3aab
MartinaZA <martina.durdikova@gmail.com>
2022-02-23 21:02:42 +0100

    1 file modified

--- promenne/ukol4.js
+++ promenne/ukol4.js
@@ -7,18 +7,5 @@ Nechte uživatele zadat 3 čísla a určete největší z nich
 let a = Number(prompt("Zadajte cislo A"));
 let b = Number(prompt("Zadajte cislo B"));
 let c = Number(prompt("Zadajte cislo C"));
-if a>b {
-  if a>c {
-    console.log(`Najvacsie cislo je ${a}`)
-  }
-  else {console.log(`Najvacie cislo je ${c}`)}
-}
-else {
-  if b>c {
-    console.log(`Najvacsie cislo je ${b}`)
-  }
-  else {
-    console.log(`Najvacsie cislo je ${c}`)
-  }
+console.log(a b c);
 
-}
