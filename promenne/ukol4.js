b095a145364eb3610e5df000b5ededb245d7df0d
MartinaZA <martina.durdikova@gmail.com>
2022-02-23 21:08:27 +0100

    1 file modified

--- promenne/ukol4.js
+++ promenne/ukol4.js
@@ -4,8 +4,22 @@ Nechte uživatele zadat 3 čísla a určete největší z nich
 "Největší číslo je A: 37".
 */
 
-let a = Number(prompt("Zadajte cislo A"));
-let b = Number(prompt("Zadajte cislo B"));
-let c = Number(prompt("Zadajte cislo C"));
-console.log(a b c);
+let a = Number ( prompt("Zadajte cislo A") );
+let b = Number ( prompt("Zadajte cislo B") );
+let c = Number ( prompt("Zadajte cislo C") );
+console.log(`A = ${a}, B = ${b}, C = ${c}`);
+if (a>b) {
+  if (a>c) {
+    console.log(`Najvacsie cislo je ${a}`)
+  }
+  else {console.log(`Najvacie cislo je ${c}`)}
+}
+else {
+  if (b>c) {
+    console.log(`Najvacsie cislo je ${b}`)
+  }
+  else {
+    console.log(`Najvacsie cislo je ${c}`)
+  }
 
+}
