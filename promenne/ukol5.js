e3cf01e758e7aa5cc6708d9fe6a4ae5a8ac5bdf7
MartinaZA <martina.durdikova@gmail.com>
2022-02-23 21:27:19 +0100

    1 file modified

--- promenne/ukol5.js
+++ promenne/ukol5.js
@@ -10,11 +10,13 @@ prikladu 2...
 let a = Number ( prompt("Zadajte dlzku strany trojuholnika A") );
 let b = Number ( prompt("Zadajte dlzku strany trojuholnika B") );
 let c = Number ( prompt("Zadajte dlzku strany trojuholnika C") );
-console.log(`A = ${a}, B = ${b}, C = ${c}`);
+console.log(`Strana trojuholnika A = ${a}, strana trojuholnika B = ${b}, strana trojuholnika C = ${c}`);
 if (a>b) {
   if (a>c) {
-    if ((b+c)>a) 
+    if ((b+c)>a) {
     console.log(`Najvacsie cislo je ${a}. Trojuholnik je mozne zostrojit.`)
+  } else {
+    console.log(`Trojuholnik nie je mozne zostrojit, pretoze sucet dvoch mensich stran je mensi ako najdlhsia strana/`)
   }
   else {console.log(`Najvacie cislo je ${c}`)}
 }
